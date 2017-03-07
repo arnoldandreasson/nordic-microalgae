287ad6d4fe4ac24542134b8d4cfc462377e05b38
Arnold Andreasson <arnold@mellifica.se>
2017-03-07 15:16:03 +0100

    Settings template added.

---
+++ pyscripts/nordicmicroalgae_settings_TEMPLATE.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Settings for MySql.
+
+MYSQL_HOST = 'localhost' 
+MYSQL_DATABASE = 'nordicmicroalgae' 
+MYSQL_USER = 'root' 
+MYSQL_PASSWORD = ''
+
+# Paths to directories and files. 
+
+PATH_TO_DOWNLOADS = ''
